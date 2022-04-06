c7987c2198d371b69c3e44f439a2c8754184be4c
believeohiozua <believe.ohiozua@gmail.com>
2022-04-06 06:00:03 +0100

    added documentation of logics

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# Django-graphQL-test
+### Django-graphQL-test
+
+This is a simple django authentication logic with GraphQL
+
+list of queries
+
+1. Signup user
+```
+query{
+   signup(
+        email:"markasgewl@gmail.com",
+        password:"12345", 
+    ){
+        email isVerified
+    }
+} 
+```
+
+
+2. login 
+```
+query{
+   login(
+        email:"markasgewl@gmail.com",
+        password:"12345", 
+    ){
+        email isVerified
+    }
+} 
+```
+
+3. Get all users
+ ```
+ query{
+   allUsers{
+        email isVerified
+    }
+}
+```
+
+4. forgotPassword 
+```
+   query{
+   forgotPassword(email:"example@example.com"){
+        email otp
+    }
+}
+    ```
+5. resetPassword 
+```
+   query{
+   resetPassword(email:"believe@holotch.com", otp:"5067", newPassword:"12345"){
+        email isVerified
+    }
+}
+    ```
---
+++ Usergraph/usermod/schema.py
@@ -0,0 +1,81 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import User
+import random
+from django.contrib.auth import authenticate
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class Query(graphene.ObjectType):
+    signup = graphene.Field(
+        UserType,
+        email=graphene.String(),
+        password=graphene.String(),
+        first_name=graphene.String(),
+        last_name=graphene.String()
+    )
+    confirm_otp = graphene.Field(
+        UserType, email=graphene.String(), otp=graphene.String())
+    login = graphene.Field(
+        UserType, email=graphene.String(), password=graphene.String())
+    all_users = graphene.List(UserType)
+    forgot_password = graphene.Field(UserType, email=graphene.String())
+    reset_password = graphene.Field(
+        UserType, email=graphene.String(), otp=graphene.String(), new_password=graphene.String())
+
+    def resolve_signup(self, info, email, password, **kwargs):
+        try:
+            user = User.objects.create_user(email=email, password=password)
+            return user
+        except Exception as error:
+            return error
+
+    def resolve_confirm_otp(self, info, email, otp, **kwargs):
+        try:
+            get_email = User.objects.get(email=email, otp=otp)
+            if get_email is not None:
+                get_email.is_verified = True
+                get_email.save()
+                return get_email
+        except Exception as error:
+            return error
+
+    def resolve_login(self, info, email, password, **kwargs):
+        try:
+            user = authenticate(username=email, password=password)
+            if user.is_verified:
+                return user
+            return Exception("User is not verified")
+        except Exception as error:
+            return error
+
+    def resolve_all_users(self, info, **kwargs):
+        return User.objects.all()
+
+    def resolve_forgot_password(self, info, email, **kwargs):
+        try:
+            get_email = User.objects.get(email=email)
+            if get_email is not None:
+                otp = random.randint(1000, 9999)
+                get_email.otp = otp
+                get_email.save()
+                return get_email
+        except Exception as error:
+            return error
+
+    def resolve_reset_password(self, info, email, otp, new_password, **kwargs):
+        try:
+            get_email = User.objects.get(email=email, otp=otp)
+            get_email.set_password(new_password)
+            return get_email
+
+        except Exception as error:
+            return error
+
+
+schema = graphene.Schema(query=Query)
